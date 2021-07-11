deb121b10ea514f139aac480c271f0f546ee5c74
changsooooooooooo <lcs940621@gmail.com>
2021-07-11 15:19:31 +0900

    spring junit test

--- spring-calculator/src/main/java/com/example/springcalculator/component/DollarCalculator.java
+++ spring-calculator/src/main/java/com/example/springcalculator/component/DollarCalculator.java
@@ -1,17 +1,16 @@
 package com.example.springcalculator.component;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class DollarCalculator implements ICalculator {
 
     private int price = 1;
-    private MarketApi marketApi;
+    private final MarketApi marketApi;
 
-    public DollarCalculator(MarketApi marketApi){
-        this.marketApi = marketApi;
-    }
-
+    @Override
     public void init(){
         this.price = marketApi.connect();
     }
